0e3d38a25fd1766e58b7b741df1d1b55e7baaf49
kalium <martijn@becker.wtf>
2017-09-19 13:17:33 +0200

    small changes

--- Javascript/child/async.js
+++ Javascript/child/async.js
@@ -4,9 +4,10 @@ const child = require('child_process')
 function main() {
 	console.log("1")
 
-	child.exec(`echo "test"`)
-		.once('close', console.log)
-		.once('exit', console.log)
+	child.exec(`echo "test" & swapjfdsakl`, console.log)
+		.once('close', (...arg) => console.log("closed:",...arg))
+		.once('exit', (...arg) => console.log("exit:",...arg))
+		.once('error', (...arg) => console.error("error:",...arg))
 		.stdout.pipe(process.stdout)
 
 	console.log("2")
