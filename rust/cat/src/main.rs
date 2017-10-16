bc278683fbbf28d8f3ae5754cb8d4ae8d2fd8d50
kaliumxyz <kalium@kalium.xyz>
2017-10-16 02:13:36 +0200

    [rust] cat functional

--- rust/cat/src/main.rs
+++ rust/cat/src/main.rs
@@ -1,16 +1,17 @@
 use std::error::Error;
-use std::io::*
-// use std::io::{self, Read, stdout};
-
+use std::env;
+use std::io::{Read};
+use std::fs::File;
 
 fn main() {
-	let wc = run().expect("Failed to run");
-	println!("{}", wc);
+	let args: Vec<String> = env::args().collect();
+	println!("{}", run(args[1].to_string()).expect("Failed to run"));
 }
 
-fn run() -> Result<usize, Box<Error>> {
+fn run(file_name: String) -> Result<String, Box<Error>> {
+	let mut file = File::open(file_name)?;
 	let mut buffer = String::new();
-	io::stdin().read_to_string(&mut buffer)?;
+	file.read_to_string(&mut buffer)?;
 
-	Ok(buffer.split_whitespace().count())
+	Ok(buffer)
 }
