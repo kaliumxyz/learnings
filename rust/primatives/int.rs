83a09ed1559f976c1efe38c17da6cb84f71d79f1
kalium <martijn@kalium.xyz>
2017-11-14 22:17:47 +0100

    wip(rust) forgot to commit

---
+++ rust/primatives/int.rs
@@ -0,0 +1,8 @@
+
+fn main() {
+	let mut int: u8 = 0;
+	loop {
+		int = int + 1;
+		println!("{:?}", int);
+	}
+}
---
+++ rust/rot13/src/main.rs
@@ -0,0 +1,13 @@
+// use std::io::{Self, }
+
+fn main() {
+	// let mut buff: String::new();
+	// io::in().read_to_string(&buff);
+	let mut int: u8 = 0;
+	loop {
+		int += 1;
+		println!("{:?}", int);
+	}
+
+
+}
